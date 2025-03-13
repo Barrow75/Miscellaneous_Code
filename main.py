c146d42513b8367b28fc5a6f374054a8607d2afb
Ray <105521700+Barrow75@users.noreply.github.com>
2025-03-13 16:33:10 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+# Prompt: Getting text files from the internet uising python
+
+
+import requests
+
+
+online_file = '[Enter link of text file from online]'
+
+request = requests.get(online_file)
+
+
+with open('[Enter File Name]', 'w', encoding='utf-8') as f:
+    f.write(request.text)
+
+
