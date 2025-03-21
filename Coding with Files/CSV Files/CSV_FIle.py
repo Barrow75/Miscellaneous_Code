59636aee799ac1b08430b2e5ddb08956dfabe7d2
Ray <105521700+Barrow75@users.noreply.github.com>
2025-03-21 02:27:12 -0600

    Update CSV_FIle.py

--- Coding with Files/CSV Files/CSV_FIle.py
+++ Coding with Files/CSV Files/CSV_FIle.py
@@ -1,1 +1,17 @@
+# reading and writing to CSV manually 
 
+import csv
+
+# writing to CSV 
+with open("example.csv", "w", newline="") as file:
+  writer = csv.writer(file)
+  writer.writerow(["name", "age", "country"])
+  writer.writerow(["Alice", 25, "USA"])
+  writer.writerow(["Bob", 30, "UK"])
+
+
+# reading CSV file
+with open("example.csv", "r") as file:
+  reader = csv.reader(file)
+  for row in reader:
+    print(row)
