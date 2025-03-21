29006b604d383a9aa491e4d26a68c232a9e73bad
Ray <105521700+Barrow75@users.noreply.github.com>
2025-03-21 02:46:27 -0600

    Update CSV_from_internet.py

--- Coding with Files/CSV Files/CSV_from_internet.py
+++ Coding with Files/CSV Files/CSV_from_internet.py
@@ -1,1 +1,36 @@
+# for data science/machine learning
+
+import pandas as pd
+
+url = "[enter url.csv]"
+df = pd.read_csv(url)
+print(df)
+
+
+
+# simply reading a CSV
+
+import requests
+import csv
+import io 
+
+# csv file url
+url_request = "[enter url.csv]"
+
+# get contents of csv
+
+# .get makes a get request to the URL requesting the file (hey get me this file)
+response = requests.get(ur_request)
+#b .text gives the entire csv file content as a string
+csv_text = response.text
+
+# stringIO treats the text as a file object
+csv_file = io.StringIO(csv_text)
+
+# read  csv using csv reader
+reader = csv.reader(csv_file)
+
+# loops through each row and prints it
+for row in reader:
+  print(row)
 
